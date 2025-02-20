9be59d6242885ec0450babd48d36c5877ad50ec4
ggmartins <ggmartins@gmail.com>
2025-02-19 19:12:26 -0600

    new switch from .net8

--- advanced-c-sharp-language-features/Experiments2/Program.cs
+++ advanced-c-sharp-language-features/Experiments2/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Experiments1
 {
+  public class Circle
+  {
+    public int Radius { get; set; }
+  }
+  public class Rectangle
+  {
+    public int Width { get; set; }
+    public int Height { get; set; }
+  }
+  public class Triangle
+  {
+    public int Base { get; set; }
+    public int Height { get; set; }
+  }
   class Program
   {
 
@@ -36,6 +50,16 @@ namespace Experiments1
       return (date is { Month: 12, Day: 1 or 25 });
     }
 
+    static string ShapeToString(Object shape) => shape switch
+    {
+      Circle { Radius: var r } when r > 10 => "Big Circle",
+      Circle { Radius: var r } when r <= 10 => "Little Circle",
+      Rectangle { Width: var w, Height: var h } when w == h => "Square",
+      Rectangle => "Rectangle",
+      Triangle => "Triangle",
+      _ => "Unknown shape"
+    };
+
     static void testBasicPatterns()
     {
       Console.WriteLine(dashedLine_wopatterns(10));
@@ -52,6 +76,12 @@ namespace Experiments1
       Console.WriteLine($"checkDate(new DateTime(2025, 12, 25) = {checkDate(new DateTime(2025, 12, 25))}");
       Console.WriteLine($"checkDate(new DateTime(2025, 12, 26) = {checkDate(new DateTime(2025, 12, 26))}"); 
       Console.WriteLine($"checkDate(null) = {checkDate(null)}");
+      Console.WriteLine(ShapeToString(new Circle { Radius = 5 }));
+      Console.WriteLine(ShapeToString(new Circle { Radius = 15 }));
+      Console.WriteLine(ShapeToString(new Rectangle { Width = 5, Height = 5 }));
+      Console.WriteLine(ShapeToString(new Rectangle { Width = 10, Height = 20 }));
+      Console.WriteLine(ShapeToString(new Triangle { Base = 10, Height = 10 }));
+      Console.WriteLine(ShapeToString(new { Width = 10, Height = 10 }));
     }
 
     public static void Main(string[] args)
