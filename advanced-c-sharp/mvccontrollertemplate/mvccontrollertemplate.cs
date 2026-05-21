fa8112df61fdf2e6f269939c81204ad968e9b6ca
ggmartins <ggmartins@gmail.com>
2026-05-21 13:29:13 -0500

    csharp templates

---
+++ advanced-c-sharp/mvccontrollertemplate/mvccontrollertemplate.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class mvccontrollertemplate : Controller
+    {
+        // GET: mvccontrollertemplate
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ advanced-c-sharp/mvctemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvctemplate.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
